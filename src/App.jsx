a312bde40a7cfed9b886a344bc41d0413983f692
Shayan <35280babu@gmail.com>
2025-02-16 13:17:34 +0600

    conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,29 @@
-import Profile from "./Component/Profile";
-import "../src/style.css";
 
-export default function App() {
-  return (
-    <>
-      <div className="mainDiv">
-        <h1>Notable Scientiests</h1>
-            <section className="sectionDesign">
-             
-              <Profile
-              name= "Maria Sklodowska-Curie"
-              person={{
-                imageUrl: "./src/Assets/Anna_frank.jpg",
-              }}
-              size={100}
-              profession="physicist and chemist"
+function Item({name,ispacked}){
+  return(
+     ispacked && <li>{name}</li> 
+  );
+}
 
-              awards= {[
-                'Nobel Prize in Physics',
-                'Nobel Prize in Chemistry',
-                'Davy Medal',
-                'Matteucci Medal'
-              ]}
+export default function App(){
+  return(
+    <section >
+      <ul>
+        <Item
+        ispacked = {true}
+        name = "Space suit"
+        > 
+        </Item>
+        <Item
+        ispacked={true}
+        name = 'Helmet with a golden leaf'
+        />
+        <Item
+        ispacked={true}
+        name=" Photo of Tom"
+        />
+      </ul>
+    </section>
 
-              discovered = 'polonium(chemical element)'
-              
-              />
-            </section>
-            <section className="sectionDesign">
-              <Profile
-              name=  "Katsuko Saruhashi"
-              person={{
-                  imageUrl: "./src/Assets/Rabindranath_Tagore.jpg"
-                }}
-                size={100}
-                profession='geochemist'
-                awards={['(Miyake Prize for geochemistry','Tanaka Prize']}
-                discovered='a method for measuring carbon dioxide in seawate'
-              />
-            </section>
-            
-
-      </div>
-    </>
   );
 }
